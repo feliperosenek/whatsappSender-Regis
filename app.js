3fa0f16071e56cbd2047a139df6816c6cf667a9e
Felipe Rosene Klapsztein <feliperosenek@gmail.com>
2022-09-15 16:53:42 -0300

    init

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const puppeteer = require("puppeteer");
-const api = require('express');
-const bodyParser = require('body-parser');
-const express = api();
-express.use(api.json())
-
+var fs = require('fs')
+const csv = require('csv-parser')
+var contatos = []
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
-
 app();
 async function app() {
   try {
@@ -22,37 +19,45 @@ async function app() {
 
     let browser = await puppeteer.launch(options);
     let page = await browser.newPage();
-
+    
     await page.goto("https://web.whatsapp.com/")
 
+    console.log("Aguardando 30 segundos para escanear QR CODE")
 
-    express.post('/', async (req,res) => {    
-    
-      console.log(req.body['mautic.form_on_submit'][0].submission.results);
-      var tel = req.body['mautic.form_on_submit'][0].submission.results.whatsapp;
-      var nome = req.body['mautic.form_on_submit'][0].submission.results.nome
+    await delay(30000)   
+        
+    var mensagem = fs.readFileSync('mensagem.txt', 'utf8');  
+    mensagem = encodeURIComponent(mensagem)
+    const results = [];
+    var log = []
+   
+    console.log("Carregando contatos")
+    fs.createReadStream('contatos.csv')
+      .pipe(csv())
+      .on('data', (data) => results.push(data))
+      .on('end', async() => {
+        results.forEach(element => {   
+          contatos.push(element.contatos)
+        });
+        await page.close()   
 
-      page2 = await browser.newPage();
-      var url = "https://web.whatsapp.com/send?phone=+55"+tel+"&text=Salve "+nome+"!!!! Primeira mensagem automatizada do Mautic no bot de Whatsapp da SouCannabis"
-   	await page2.goto(url)
-      await delay(8000)
-  
-      var verifyNumber = await page2.$("._2Nr6U")
-      if (verifyNumber == null) {
-        await page2.click("button[aria-label='Enviar']")
+        for (let i = 0; i < contatos.length; i++) {
+          console.log("Enviando mensagem para: "+contatos[i])          
+           page2 = await browser.newPage();
+           await page2.goto("https://web.whatsapp.com/send?phone=+55"+contatos[i]+"&text="+mensagem, { waitUntil: "networkidle0" })                 
+           await delay(8000)
+           var verifyNumber = await page2.$("._2Nr6U")
+
+           if(verifyNumber == null){
+           await page2.click("button[aria-label='Enviar']") 
+          }
+           await delay(3000)
+           await page2.close()
       }
-  
-      await delay(3000)
-      await page2.close()
-    })
-
-  
-
+    });  
+ 
+       
   } catch (error) {
     console.log(error);
   }
 }
-
-express.listen(3000, () => {
-  console.log("Express ON");
-})
